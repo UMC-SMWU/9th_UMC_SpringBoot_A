1bae93271a7736241c30f0645e2fd9b37b7ba9ae
yoojinche <chesunny917@gmail.com>
2025-10-05 22:03:38 +0900

    🚀 chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/umc9th/umc9th/Umc9thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc9th.umc9th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc9thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
