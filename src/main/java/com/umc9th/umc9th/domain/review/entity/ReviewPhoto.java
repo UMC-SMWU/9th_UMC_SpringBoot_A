58253b7ba2229f273eb0cc03c4f2c1061257f491
yoojinche <chesunny917@gmail.com>
2025-10-10 17:00:16 +0900

    ✨feat: review_photo 어노테이션 추가

--- src/main/java/com/umc9th/umc9th/domain/review/entity/ReviewPhoto.java
+++ src/main/java/com/umc9th/umc9th/domain/review/entity/ReviewPhoto.java
@@ -1,4 +1,18 @@
 package com.umc9th.umc9th.domain.review.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "review_photo")
 public class ReviewPhoto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
