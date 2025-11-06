e59bdf76f5e258bdb0238ab5facab51df97af495
yoojinche <chesunny917@gmail.com>
2025-11-06 16:29:45 +0900

    ✨feat: leftJoin 기능 추가

--- src/main/java/com/umc9th/umc9th/domain/review/repository/ReviewQueryDslImp.java
+++ src/main/java/com/umc9th/umc9th/domain/review/repository/ReviewQueryDslImp.java
@@ -4,6 +4,8 @@ import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.umc9th.umc9th.domain.review.entity.QReview;
 import com.umc9th.umc9th.domain.review.entity.Review;
+import com.umc9th.umc9th.domain.store.entity.QLocation;
+import com.umc9th.umc9th.domain.store.entity.QStore;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -26,12 +28,14 @@ public class ReviewQueryDslImp implements ReviewQueryDsl{
 
         // Q클래스 선언
         QReview review = QReview.review;
+        QStore store = QStore.store;
+        QLocation location = QLocation.location;
 
         return queryFactory
                 .selectFrom(review)
+                .leftJoin(store).on(store.id.eq(review.store.id))
+                .leftJoin(location).on(location.id.eq(store.location.id))
                 .where(predicate)
                 .fetch();
-
-
     }
 }
